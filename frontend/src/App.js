27fc1145f29b50c25d0acde945793c90182326e5
Muhammad Mohad <l230784@lhr.nu.edu.pk>
2025-02-02 12:37:47 +0500

    1.0.0

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import React, {useState, useEffect} from 'react';
+import './App.css'
+
+function App(){
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch('http://localhost:5000')
+    .then((response) => response.text())
+    .then((data) => setMessage(data));
+  }, []);
+
+  return (
+    <div>
+      <p id='main'>Good Things Take Time</p>
+    </div>
+  );
+}
+export default App;
